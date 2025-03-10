e0e5c3fc6622fd572b6972da5a8fdbb2e45e29d4
dochmai382 <dochmai382@gmail.com>
2025-03-10 15:37:42 +0900

    model 변경

--- src/main/java/org/example/ex10/model/ModelType.java
+++ src/main/java/org/example/ex10/model/ModelType.java
@@ -2,7 +2,7 @@ package org.example.ex10.model;
 
 public enum ModelType {
     GROQ_LLAMA("llama-3.3-70b-versatile", ModelPlatform.GROQ),
-    TOGETHER_LLAMA("meta-llama/Llama-3.3-70B-Instruct-Turbo", ModelPlatform.TOGETHER)
+    TOGETHER_LLAMA("meta-llama/Llama-3.3-70B-Instruct-Turbo-Free", ModelPlatform.TOGETHER)
     ;
 
     public final String name;
